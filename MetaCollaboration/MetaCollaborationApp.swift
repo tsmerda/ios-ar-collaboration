315d21fcba725ce2ca6093a8fc9a1a37c4f35fee
Tomáš Šmerda <tomas.smerda@adbros.cz>
2023-01-22 15:58:16 +0000

    Initial Commit

---
+++ MetaCollaboration/MetaCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaCollaborationApp.swift
+//  MetaCollaboration
+//
+//  Created by Tomáš Šmerda on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetaCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
